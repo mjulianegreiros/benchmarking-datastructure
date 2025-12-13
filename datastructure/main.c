e45be5abbc0f835f9b57410ae898364437c963e7
JadielSilva11 <jdielcunha@gmail.com>
2025-12-13 15:29:06 -0300

    Slr

--- datastructure/main.c
+++ datastructure/main.c
@@ -5,7 +5,7 @@
 #include "hash/tabelaHash.h"
 #include "abb/arvore.h"
 #include "avl/avl.h"
-#include "benchmark/benchmark.h"
+#include "../benchmark/benchmark.h"
 
 #define TAM 50000 // aqui é o tamanho dos vetores das tabelas hashs
 #define VAZIO 0;
@@ -23,17 +23,7 @@ int main(){
     int tempoInsereHash=0 , tempoRemoveHash=0 , tempoBuscaHash=0;
     int tempoInsereHashEND=0 , tempoRemoveHashEND=0 , tempoBuscaHashEND=0;
 
-    /*int rotacoes = 0;
-    Avl *a = criar_avl();
-    inserir_avl(a, 3, &rotacoes);
-    inserir_avl(a, 1, &rotacoes);
-    inserir_avl(a, 2, &rotacoes);
-    printf("%d\n", rotacoes);*/
 
-    // aqui deve ter um menu para escolher qual amostra deve ser inserida.
-    // são necessarios 3 arquivos, um pra cada amostra
-    // exemplo: tabela hash
-    // abre o arquivo, e para a inserção:
     int vet[TAM];
     for(int i=0;i<TAM;i++){
         tempoInsereHash += calcTempoInsereHash(num , vet , TAM , &colisoes);
